b253b64267920e2c8c0bd66f4dcbf73bd13b7449
Moein <moein.khazraee@gmail.com>
2022-02-27 18:18:14 -0500

    Added round to even number of bins to fft_bin_calculator.

--- utilities/fft_bin_calculator.c
+++ utilities/fft_bin_calculator.c
@@ -5,12 +5,12 @@
 const int rnd_digits = 4;
 
 struct exact_ranges {
-		float        l_freq;
-		float        r_freq;
-		unsigned int l_bin1;
-		unsigned int r_bin1;
-		unsigned int l_bin2;
-		unsigned int r_bin2;
+    float        l_freq;
+    float        r_freq;
+    unsigned int l_bin1;
+    unsigned int r_bin1;
+    unsigned int l_bin2;
+    unsigned int r_bin2;
 };
 
 float round_float (float val, int rnd_digits){
@@ -18,13 +18,14 @@ float round_float (float val, int rnd_digits){
 }
 
 int bins_calc (float capture_center_freq, float capture_bw,
-								float band_center_freq, float band_bandwidth,
-								float filter_bw, unsigned int fft_size,
-								struct exact_ranges * final_ranges){
+               float band_center_freq, float band_bandwidth,
+               float filter_bw, unsigned int fft_size,
+               struct exact_ranges * final_ranges){
 
-	  int   half_fft, left_bin, right_bin;
+    int   half_fft, left_bin, right_bin;
     float bin_width, capture_left, filter_left, filter_right,
-		      band_left, band_right;
+          band_left, band_right, left_bin_float, right_bin_float,
+          l_adj, r_adj;
 
     // Compute frequency ranges
     bin_width    = capture_bw / fft_size;
@@ -35,83 +36,100 @@ int bins_calc (float capture_center_freq, float capture_bw,
     band_right   = band_center_freq    + (band_bandwidth /2);
 
     // Frequency range check
-	  if ((band_left<filter_left) || (band_right>filter_right)){
+    if ((band_left<filter_left) || (band_right>filter_right)){
         return 0;
-	  }
+    }
 
     // Compute FFT bin number, round to avoid float errors
-    band_left  = round_float((band_left  - capture_left) / bin_width, rnd_digits);
-    band_right = round_float((band_right - capture_left) / bin_width, rnd_digits);
+    left_bin_float  = round_float((band_left  - capture_left) / bin_width, rnd_digits);
+    right_bin_float = round_float((band_right - capture_left) / bin_width, rnd_digits);
 
     // find the integer values for bin number
-    left_bin = floor(band_left);
-    if (ceil(band_right)==floor(band_right))
-        right_bin = floor(band_right) - 1;
+    left_bin = floor(left_bin_float);
+    if (ceil(right_bin_float)==floor(right_bin_float))
+        right_bin = floor(right_bin_float) - 1;
     else
-        right_bin = floor(band_right);
+        right_bin = floor(right_bin_float);
+
+    // Make number of bins an even number for overlapping
+    l_adj = round_float (band_left - (capture_left + left_bin*bin_width),
+		                     rnd_digits);
+    r_adj = round_float ((capture_left + (right_bin+1)*bin_width) - band_right,
+		                     rnd_digits);
+
+    if ((right_bin-left_bin+1)%2 != 0){
+        if ((left_bin == 0) || (left_bin == (fft_size/2)))
+            right_bin += 1;
+        else if ((right_bin == (fft_size-1)) || (right_bin == (fft_size/2)-1))
+            left_bin -= 1;
+        else if (r_adj > l_adj)
+            right_bin -= 1;
+        else // if (r_adj <= l_adj)
+            right_bin += 1;
+    }
 
     // Frequency range captured
     final_ranges->l_freq = round_float(capture_left + left_bin     *bin_width, rnd_digits);
     final_ranges->r_freq = round_float(capture_left + (right_bin+1)*bin_width, rnd_digits);
 
     // FFT half window shift, so center frequency is at bin 0
-	  half_fft  = fft_size/2;
+    half_fft  = fft_size/2;
     left_bin  = left_bin ^ half_fft;
     right_bin = right_bin ^ half_fft;
 
     // check if the range is continuous or not
     if (((left_bin < half_fft    ) && (right_bin < half_fft    )) ||
-	      ((left_bin > (half_fft-1)) && (right_bin > (half_fft-1)))){
+        ((left_bin > (half_fft-1)) && (right_bin > (half_fft-1)))){
 
-				final_ranges-> l_bin1 = left_bin;
-				final_ranges-> r_bin1 = right_bin;
-				final_ranges-> l_bin2 = 0;
-				final_ranges-> r_bin2 = 0;
-				return 1;
+        final_ranges-> l_bin1 = left_bin;
+        final_ranges-> r_bin1 = right_bin;
+        final_ranges-> l_bin2 = 0;
+        final_ranges-> r_bin2 = 0;
+        return 1;
 
     } else if ((left_bin == half_fft) && (right_bin == (half_fft-1))) {
 
-				final_ranges-> l_bin1 = 0;
-				final_ranges-> r_bin1 = fft_size-1;
-				final_ranges-> l_bin2 = 0;
-				final_ranges-> r_bin2 = 0;
-				return 1;
+        final_ranges-> l_bin1 = 0;
+        final_ranges-> r_bin1 = fft_size-1;
+        final_ranges-> l_bin2 = 0;
+        final_ranges-> r_bin2 = 0;
+        return 1;
 
     } else {
 
-				final_ranges-> l_bin1 = 0;
-				final_ranges-> r_bin1 = right_bin;
-				final_ranges-> l_bin2 = left_bin;
-				final_ranges-> r_bin2 = fft_size-1;
-				return 2;
+        final_ranges-> l_bin1 = 0;
+        final_ranges-> r_bin1 = right_bin;
+        final_ranges-> l_bin2 = left_bin;
+        final_ranges-> r_bin2 = fft_size-1;
+        return 2;
 
-		}
+    }
 }
 
 int main( int argc, char *argv[] ) {
-		struct exact_ranges ranges;
-		int    range_count;
+    struct exact_ranges ranges;
+    int    range_count;
 
-	  if (argc!=4) {
+    if (argc!=4) {
         printf("Missing arguments: <capture center freq> <band center freq> <band bandwidth>\n");
-	  } else {
-				// Pluto:
-				// range_count = bins_calc(atof(argv[1]), 61.44, atof(argv[2]), atof(argv[3]), 56.0, 1024, ranges);
-				// N210+WBX:
-				// range_count = bins_calc(atof(argv[1]), 100.0, atof(argv[2]), atof(argv[3]), 40.0, 2048, ranges);
-				// N210+SBX:
-				range_count = bins_calc(atof(argv[1]), 100.0, atof(argv[2]), atof(argv[3]), 100.0, 2048, &ranges);
+    } else {
+        // Pluto:
+        range_count = bins_calc(atof(argv[1]), 61.44, atof(argv[2]), atof(argv[3]), 56.0, 1024, &ranges);
+        // N210+WBX:
+        // range_count = bins_calc(atof(argv[1]), 100.0, atof(argv[2]), atof(argv[3]), 40.0, 2048, &ranges);
+        // N210+SBX:
+        // range_count = bins_calc(atof(argv[1]), 100.0, atof(argv[2]), atof(argv[3]), 100.0, 2048, &ranges);
 
-				if (range_count == 0){
-						printf ("Band frequency out of filter range.\n");
-				} else if (range_count == 1) {
-						printf ("Frequency range: %.*f to %.*f\n", rnd_digits, ranges.l_freq, rnd_digits, ranges.r_freq);
-						printf ("FFT range      : %d to %d\n", ranges.l_bin1, ranges.r_bin1);
-				} else {
-						printf ("Frequency range: %.*f to %.*f\n", rnd_digits, ranges.l_freq, rnd_digits, ranges.r_freq);
-						printf ("FFT ranges     : %d to %d and %d to %d\n", ranges.l_bin1, ranges.r_bin1, ranges.l_bin2, ranges.r_bin2);
-				}
-		}
+        if (range_count == 0){
+            printf ("Band frequency out of filter range.\n");
+        } else if (range_count == 1) {
+            printf ("Frequency range: %.*f to %.*f\n", rnd_digits, ranges.l_freq, rnd_digits, ranges.r_freq);
+            printf ("FFT range      : %d to %d\n", ranges.l_bin1, ranges.r_bin1);
+        } else {
+            printf ("Frequency range: %.*f to %.*f\n", rnd_digits, ranges.l_freq, rnd_digits, ranges.r_freq);
+            printf ("FFT ranges     : %d to %d and %d to %d\n", ranges.l_bin1, ranges.r_bin1, ranges.l_bin2, ranges.r_bin2);
+        }
+    }
 
-	  return 0;
+    return 0;
 }
